b0ee08377cd6d328e81ba5c7dca400d804b05eb3
sailinga <sailinga@DIN73003065.corp.capgemini.com>
2018-11-26 09:52:23 +0530

    commit

--- demo1/src/main/java/demo1/GitDemo.java
+++ demo1/src/main/java/demo1/GitDemo.java
@@ -3,9 +3,11 @@ package demo1;
 public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("hi welcome ");
+		System.out.println("this is branching example");
 	}
 	public static void greeting() {
 		System.out.println("welcome to git");
+		System.out.println("this is branching example");
 		System.out.println("this is branching example");
 	}
 
